3bc28cba713652e74a2e6f833e386c8c06382fa1
waaand <waaand14@student.westerdals.no>
2017-05-09 00:34:26 +0200

    Added route to root page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get 'filter_page/method'
 
-  get 'front_page/method'
+  # get 'front_page/method'
+
+  root 'front_page/method', as: 'front_page_method'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
