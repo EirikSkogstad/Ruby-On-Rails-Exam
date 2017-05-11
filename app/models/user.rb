d107d4f776d51f6ef6c90bab3b4b80b45e62d24c
zenix <ze9ix@live.no>
2017-05-11 08:12:22 +0200

    Added validation for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  validates :username, :password_digest, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 8, maximum: 50}
+
   has_secure_password
+
 end
