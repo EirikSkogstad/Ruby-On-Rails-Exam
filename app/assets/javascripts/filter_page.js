e5a5c7e7b455fa8525d5d6eaea050347662686e4
Opsgar15 <opsgar15@student.westerdals.no>
2017-06-02 14:41:40 +0200

    Structured buttons and added pseudocode for later implentation

--- app/assets/javascripts/filter_page.js
+++ app/assets/javascripts/filter_page.js
@@ -1,3 +1,30 @@
+filters = [];
+
+//On buttonclick add the category to the filters-list
+
 $('.btn').click(function() {
-    $(this).toggleClass('blue-grey lighten-2 blue-grey');
+    //Either do btn.id or btn.text.toLowerCase(). Whatever is best
+    //var btn = btn.id
+    //if(filters.contains(btn){
+    //    remove(btn)
+    // } else{
+    //    filters.push(btn)
+    // }
+
+    $(this).toggleClass('blue-grey darken-3 blue-grey');
 });
+
+// Redirect to frontpage with different movie-list based on buttons pressed.
+$('#big-button').click(function() {
+    /*
+    var foundMovies = [];
+    $.each(allMovies, function(movieIndex, movie) {
+        $.each(filters, function(filterIndex, filter) {
+            if (movie.category.indexOf(filter) >= 0) {
+                foundMovies.push(movie);
+            }
+        });
+    });
+    */
+});
+
