e0e74127b80ab1a4792125ef734ee23860091577
zenix <ze9ix@live.no>
2017-06-03 20:33:57 +0200

    Lowercase validation for username. Both in model and login field. (Should work now!)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,16 @@ class SessionsController < ApplicationController
   include SessionsHelper
 
   def create
-    user = User.find_by(username: params[:name])
+    username = params[:name]
+    username = username.downcase
+    user = User.find_by(username: username)
     if user and user.authenticate(params[:password])
       session[:username] = user.username
 
       redirect_to front_page_path
     else
       flash[:notice] = 'Wrong username or password. Please try again'
-      redirect_to new_user_path
+      redirect_to front_page_path
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :username, :password_digest, presence: true
   validates :username, uniqueness: true
+  #make sure ONLY lower case is accepted
+  validates :username, format: {with: /\A[a-z0-9]+\z/, :message => ' cannot be uppercase, and must contains letters from a-z and/or numbers'}
   validates :password, length: {minimum: 8, maximum: 50}
 
   has_secure_password
